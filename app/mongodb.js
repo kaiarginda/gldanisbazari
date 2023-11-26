289e5d02d840d8bbea90e5528a8ab6fcb0887b13
tornike <tornike.tanda@gmail.com>
2023-11-26 04:56:49 -0800

    heil hitler123445

--- app/mongodb.js
+++ app/mongodb.js
@@ -1,11 +1,31 @@
+// import mongoose from "mongoose";
+
+// export const connectMongoDB = async () => {
+//   const DB =
+//     "mongodb+srv://tornike:tornike@shopcluster.bqft7gb.mongodb.net/product?retryWrites=true&w=majority";
+//   mongoose
+//     .connect(DB, {
+//       useNewUrlParser: true,
+//     })
+//     .then(() => console.log("DB connnection successful!"));
+// };
+
 import mongoose from "mongoose";
 
-export const connectMongoDB = async () => {
-  const DB =
-    "mongodb+srv://tornike:tornike@shopcluster.bqft7gb.mongodb.net/product?retryWrites=true&w=majority";
-  mongoose
-    .connect(DB, {
-      useNewUrlParser: true,
-    })
-    .then(() => console.log("DB connnection successful!"));
+// Assuming `MONGO_URI` is your MongoDB connection string
+const MONGO_URI =
+  "mongodb+srv://tornike:tornike@shopcluster.bqft7gb.mongodb.net/product?retryWrites=true&w=majority";
+
+const connectMongoDB = async () => {
+  await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    poolSize: 10, // Adjust this based on your needs
+  });
+
+  console.log("DB connection successful!");
 };
+
+export default connectMongoDB;
