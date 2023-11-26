9641c061cefa58e9d64ad04dc6de195790703cb4
tornike <tornike.tanda@gmail.com>
2023-11-26 04:51:02 -0800

    heil hitler123445

--- app/api/getproducts/route.jsx
+++ app/api/getproducts/route.jsx
@@ -2,7 +2,12 @@ import { connectMongoDB } from "../../mongodb";
 import Product from "../../models/Product.js";
 import { NextRequest, NextResponse } from "next/server";
 export async function GET() {
-  await connectMongoDB();
-  const products = await Product.find();
-  return new Response(JSON.stringify(products));
+  try {
+    // Your MongoDB query here
+    await connectMongoDB();
+    const products = await Product.find();
+    return new Response(JSON.stringify(products));
+  } catch (error) {
+    console.error("Error executing MongoDB query:", error);
+  }
 }
